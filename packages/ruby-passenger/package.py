f3537af98566e2bfd718bbec159c7b5d640aa082
Marc Mengel <mengel@fnal.gov>
2024-03-13 10:46:29 -0500

    updating versions

--- packages/ruby-passenger/package.py
+++ packages/ruby-passenger/package.py
@@ -12,9 +12,10 @@ class RubyPassenger(Package):
     homepage = "https://rubygems.org/gems/passenger/versions/6.0.4"
     url      = "https://rubygems.org/downloads/passenger-6.0.4.gem"
 
+    version("6.0.20", sha256="6383431c00001d3273b1038423af0516a355c521834973773f852613ee91a393", expand=False)
     version('6.0.5', sha256='9c6f406dd00c4a7c1169a235e454fe40e82ead83c775f488e0d34e809bcb5b76', expand=False)
 
-    version('6.0.4', '298806191e4e8995dbc0f94c3791b2b1916ca50b5993e04236df6f4108bfc2af', expand=False)
+    version('6.0.4', sha256='298806191e4e8995dbc0f94c3791b2b1916ca50b5993e04236df6f4108bfc2af', expand=False)
     version('5.1.11', sha256='10fb4b685a3b1eae6445e49a2aa6f9deb67ce39143c5bd3771ce599063dba159', expand=False)
 
     extends('ruby')
